675446dc45b4ecd6a51456b210858ce0a4f7fecc
Madhav Tank <102796161+madhavtank@users.noreply.github.com>
2023-07-11 17:55:42 +0530

    Create index.js

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const navLinksdata = [
+  {
+    _id: 1001,
+    title: "Home",
+    link: "home",
+  },
+  {
+    _id: 1003,
+    title: "Projects",
+    link: "projects",
+  },
+  {
+    _id: 1004,
+    title: "Resume",
+    link: "resume",
+  },
+];
