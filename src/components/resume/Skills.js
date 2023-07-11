84dcddc845f0f259b1d4d78bfa557602f8809293
Madhav Tank <102796161+madhavtank@users.noreply.github.com>
2023-07-11 18:30:32 +0530

    Create Skills.js

---
+++ src/components/resume/Skills.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+const Skills = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
+    >
+      <div className="w-full lgl:w-1/2">
+        <div className="py-12 font-titleFont flex flex-col gap-4">
+          {/* <p className="text-sm text-designColor tracking-[4px] uppercase">
+            Tech Stack
+          </p> */}
+          <h2 className="text-3xl md:text-4xl font-bold">Proficient</h2>
+        </div>
+        <div className='className="mt-14 w-full flex flex-col gap-6'>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">C/C++</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[100%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">100%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Python</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">90%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">HTML, CSS, Bootstrap</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[90%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">90%</span>
+              </motion.span>
+            </span>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-sm uppercase font-medium">Javascript</p>
+            <span className="w-full h-2 bgOpacity rounded-md inline-flex mt-2">
+              <motion.span
+                initial={{ x: "-100%", opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className="w-[95%] h-full bg-gradient-to-r from-blue-600 via-pink-500 to-red-500 rounded-md relative"
+              >
+                <span className="absolute -top-7 right-0">95%</span>
+              </motion.span>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full lgl:w-1/2">
+        <div className="py-12 font-titleFont flex flex-col gap-4">
+          {/* <p className="text-sm text-designColor tracking-[4px] uppercase">
+            Tech Stack
+          </p> */}
+          <h2 className="text-3xl md:text-4xl font-bold">Familiar</h2>
+        </div>
+        <div className="flex flex-col gap-6">
+          <div className="overflow-x-hidden">
+            <p className="text-md uppercase font-medium">React JS</p>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-md uppercase font-medium">Node JS</p>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-md uppercase font-medium">Express JS</p>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-md uppercase font-medium">Docker & Nginx</p>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-md uppercase font-medium">Mongodb and MySQL</p>
+          </div>
+          <div className="overflow-x-hidden">
+            <p className="text-md uppercase font-medium">Git</p>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default Skills
